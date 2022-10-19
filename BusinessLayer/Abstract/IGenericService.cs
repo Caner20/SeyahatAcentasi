107898eeb6a49c7743f974730feca07a4da9bbae
Caner Çömlek <211809050@ogr.cbu.edu.tr>
2022-10-19 15:35:01 +0300

    Business Layer  (Interface and Class)

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+        //Bu Interface'in amacı Business'ta tanımlanan bütün metotlar
+       //GenericService'den geçicek Business'te Concrete'e gelicek
+    public interface IGenericService<T>
+    {
+        void TAdd(T t);
+        void TDelete(T t);
+        void TUpdate(T t);
+        List<T> TGetList();
+        T GetTByID(int id);
+
+    }
+}
---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _abaoutDal;
+        public AboutManager(IAboutDal aboutDal)
+        {
+            //IAboutDal dan parametre alıyoruz(içerisinde IGenericDal'dan kalıtım alıyor)
+           //_aboutDal ile IGenericDal'ın içerisindeki metotları kullanabiliriz
+            _abaoutDal = aboutDal; 
+        }
+
+        public About GetTByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(About t)
+        {
+            _abaoutDal.Insert(t);
+        }
+
+        public void TDelete(About t)
+        {
+            _abaoutDal.Delete(t);
+        }
+
+        public List<About> TGetList()
+        {
+            return _abaoutDal.GetList();
+        }
+
+        public void TUpdate(About t)
+        {
+            _abaoutDal.Update(t);
+        }
+    }
+}
