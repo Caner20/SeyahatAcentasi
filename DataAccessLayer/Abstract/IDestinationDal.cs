4a374bdf10b9ce73bb9799afeef2097bcfcc1b70
Caner Çömlek <211809050@ogr.cbu.edu.tr>
2022-10-19 14:16:27 +0300

    Generic Interface Oluşturma

---
+++ DataAccessLayer/Abstract/IDestinationDal.cs
@@ -0,0 +1,16 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    // Kalıtımla Generic Interface'den yararlanıyoruz
+    public interface IDestinationDal :IGenericDal<Destination>
+    {
+       
+
+    }
+}
