ba8f10d9974643a7fc71b6e67ff4d12a2089d143
Caner Çömlek <211809050@ogr.cbu.edu.tr>
2022-10-26 23:41:15 +0300

    SubAbout-Testimonial-Feature Düzenleme

---
+++ SeyahatAcentasi/ViewComponents/Default/_StatisticsPartial.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeyahatAcentasi.ViewComponents.Default
+{
+    public class _StatisticsPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // ViewBag ....  ViewBag veri yazdırma 
+            using var c = new Context();
+            ViewBag.v1 = c.Destinations.Count(); //c nesnesine rotaları say
+            ViewBag.v2 = c.Guides.Count();
+            ViewBag.v3 = "352";
+            return View();
+        }
+    }
+}
