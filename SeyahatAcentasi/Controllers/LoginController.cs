fc22c449d1a84d83e0dcb8e8a2e10ec345f92d54
Caner Çömlek <211809050@ogr.cbu.edu.tr>
2022-10-31 19:23:51 +0300

    Register-Login Sayfaları

---
+++ SeyahatAcentasi/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeyahatAcentasi.Controllers
+{
+    [AllowAnonymous] //altında bulundugu ne varsa gecerli butun kurallardan muaf haline getiriyor
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
