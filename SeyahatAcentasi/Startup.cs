c4705809053fbf545f0e75ab9166154fbe02f5c2
Caner ÇÖMLEK <c.comlek@hotmail.com>
2022-12-06 21:40:10 +0300

    Admin Areas

--- SeyahatAcentasi/Startup.cs
+++ SeyahatAcentasi/Startup.cs
@@ -80,6 +80,14 @@ namespace SeyahatAcentasi
                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
             });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
