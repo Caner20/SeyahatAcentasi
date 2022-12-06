235424bf4e8f278d4126f371900e2692e0198bec
Caner ÇÖMLEK <c.comlek@hotmail.com>
2022-12-06 19:16:11 +0300

    Tur Rehberlerin Listelenmesi

---
+++ SeyahatAcentasi/ViewComponents/MemberDashBoard/_GuideList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeyahatAcentasi.ViewComponents.MemberDashBoard
+{
+    public class _GuideList : ViewComponent
+    {
+        GuideManager guideManager = new GuideManager(new EfGuideDal());  
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = guideManager.TGetList();  //guideM. 'daki bilgileri getlist()'le dondurduk  
+            return View(values);
+        }
+    }
+}
---
+++ SeyahatAcentasi/ViewComponents/MemberDashBoard/_ProfileInformation.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SeyahatAcentasi.ViewComponents.MemberDashBoard
+{
+    public class _ProfileInformation : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task <IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);//_usermanager da buluyor kullanici identity'deki name gore
+            ViewBag.memberName = values.UserName + " " + values.Surname;  //values'ten gelen ad  ve soyad a gore kullanici ismine atiyor
+            ViewBag.memberPhone = values.PhoneNumber;
+            ViewBag.memberMail = values.Email;
+            return View();
+        }
+    }
+}
